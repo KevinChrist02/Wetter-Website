1be9d07f4f8bf2dff97fc2cd27b8436ce7aab526
Kevin Christ <kevin.christ@stud.uni-heidelberg.de>
2024-07-15 09:42:23 +0200

    added the js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import { fetchWeatherApi } from 'openmeteo';
+import { fetchWeatherApi } from "openmeteo";
+
+const weather = document.querySelector(".weather");
+const locationInput = document.querySelector("#location");
+const button = document.querySelector("#submit");
+
+button.addEventListener("click", async () => {
+  const location = locationInput.value;
+  if (location) {
+    const coordinates = await GeolocationCoordinates(location);
+    if (coordinates) {
+      const weatherData = fetchWeatherApi(
+        coordinates.latidute,
+        coordinates.longitude
+      );
+      displayWeather(weatherData);
+    } else {
+      weather.innerHTML = "<p>Ort nicht gefunden</p>";
+    }
+  }
+});
